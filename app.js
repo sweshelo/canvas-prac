830df0e4fcd4c4a90ee9b4b2cf62f7676fe6d612
sweshelo <sweshelo@dmats97.xyz>
2021-06-26 01:12:46 +0900

    drag & drop

--- app.js
+++ app.js
@@ -7,13 +7,42 @@
 
     console.log(ww, wh);
 
-    var canvas = new createjs.Stage("app");
+    var stage = new createjs.Stage("app");
+
+    function cardClick(obj){
+        console.log("show effect window");
+    }
+
+    function cardMove(obj){
+        obj.x = stage.mouseX;
+        obj.y = stage.mouseY;
+    }
+
+    function cardRelease(obj){
+        obj.x = 0;
+        obj.y = 0;
+    }
+
     var img = new createjs.Bitmap("./cards/10001.jpg");
-    img.onload = ()=>{canvas.update()};
-    canvas.addChild(img);
+    img.scaleX = 0.2;
+    img.scaleY = 0.2;
+    img.addEventListener("mousedown", ()=>{cardClick(img)});
+    img.addEventListener("pressmove", ()=>{cardMove(img)});
+    img.addEventListener("pressup", ()=>{cardRelease(img)});
+    stage.addChild(img);
+
+    const handleTick = ()=>{
+        stage.update();
+        console.log(stage.mouseX, stage.mouseY);
+    }
+
+    createjs.Ticker.framerate = 60;
+    createjs.Ticker.addEventListener("tick", handleTick);
 
 })();
 
 function loadImage(path){
     const img = new createjs.Bitmap(path);
 }
+
+
